475f0dabbcbe286b185ecf9da00d0cd4253a40cf
TheDarkFever <86383084+TheDarkFever@users.noreply.github.com>
2022-08-17 03:21:25 +0530

    Add files via upload

---
+++ submaster.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+figlet -f slant SUB MASTER | lolcat
+
+echo "                              Created by Ekansh Choudhary" | lolcat
+
+echo "1. Enumerating Sub-Domians"
+
+assetfinder $1 >  $1_subdomains.txt
+
+echo "2. Sub-Domain Enumeration Completed"
+
+echo "3. Attacking Sub-domains"
+
+subzy -targets $1_subdomains.txt >  $1_takeovers.txt
+
+#wordcount = "cat $1_takeovers.txt | wc -l" 
+
+#echo "$wordcount"
+
+echo "--Analyse the results--"
+cat $1_takeovers.txt | grep "VULNERABLE"
+
